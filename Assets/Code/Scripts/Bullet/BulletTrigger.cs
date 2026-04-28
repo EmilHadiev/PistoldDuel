e9684cb7a8038c0d323fd1dbe1db7fb560b63b27
EmilKhadiev <newemil00@mail.ru>
2026-04-28 17:51:31 +0300

    fix bulletTrigger

--- Assets/Code/Scripts/Bullet/BulletTrigger.cs
+++ Assets/Code/Scripts/Bullet/BulletTrigger.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class BulletTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Столкновение!");
         gameObject.SetActive(false);
     }
 }
